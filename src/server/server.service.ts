6b8e7eba057e16d7db3d854a3479ee0c3d9919f4
dominiq007 <dom.rajkowski@gmail.com>
2023-06-03 18:59:14 +0200

    feat: improve server

--- src/server/server.service.ts
+++ src/server/server.service.ts
@@ -4,17 +4,41 @@ import { serve } from '@std/http/mod.ts';
 export class Server {
   public async start(): Promise<void> {
     await load({
+      allowEmptyValues: true,
       envPath: `${Deno.cwd()}/.env`,
       export: true,
-      allowEmptyValues: true,
     });
+
+    const minimumDenoVersion = '1.34.0';
+
+    const satisfiesDenoVersion = Deno.version.deno
+      .localeCompare(minimumDenoVersion, undefined, {
+        numeric: true,
+        sensitivity: 'base',
+      });
+
+    if (satisfiesDenoVersion === -1) {
+      console.warn(
+        `%cFlavor requires Deno version ${minimumDenoVersion} or higher %c[run 'deno upgrade' to update Deno]`,
+        `color: orange; font-weight: bold;`,
+        'color: gray;',
+      );
+
+      Deno.exit(1);
+    }
 
     await serve((request: Request) => {
       const timerStart = performance.now();
 
       const timerEnd = performance.now();
 
-      console.log(`%cRequest: ${new URL(request.url).pathname} %c[${(timerEnd - timerStart).toFixed(3)}ms]`, 'color: green; font-weight: bold;', 'color: gray;');
+      console.log(
+        `%cRequest: ${new URL(request.url).pathname} %c[${
+          (timerEnd - timerStart).toFixed(3)
+        }ms]`,
+        'color: green; font-weight: bold;',
+        'color: gray;',
+      );
 
       return new Response('Hello World!', {
         headers: {
@@ -23,6 +47,15 @@ export class Server {
       });
     }, {
       port: Number(Deno.env.get('PORT')),
+      onListen: () => {
+        console.log(
+          `%cServer listening on http://localhost:${Deno.env.get('PORT')} %c[${
+            Deno.build.os === 'darwin' ? '⌃C' : 'ctrl+c'
+          } to quit]`,
+          'color: green; font-weight: bold;',
+          'color: gray;',
+        );
+      },
     });
   }
 }
