4140b8ae1123ad32019940ee9034adf8767b5307
dominiq007 <dom.rajkowski@gmail.com>
2023-06-12 21:09:00 +0200

    feat: add basic app config api

--- src/server/interfaces/server_options.interface.ts
+++ src/server/interfaces/server_options.interface.ts
@@ -1,6 +1,8 @@
+import { AppConfig } from './app_config.interface.ts';
 import { Constructor } from '../../utils/interfaces/constructor.interface.ts';
 import { Module } from '../interfaces/module.interface.ts';
 
 export interface ServerOptions {
+  config?: AppConfig;
   modules: Constructor<Module>[];
 }
--- src/server/server.service.ts
+++ src/server/server.service.ts
@@ -2,6 +2,7 @@ import { dirname } from '@std/path/mod.ts';
 import { existsSync } from '@std/fs/mod.ts';
 import { load as loadEnv } from '@std/dotenv/mod.ts';
 import { parse as parseFlags } from '@std/flags/mod.ts';
+import { AppConfig } from './interfaces/app_config.interface.ts';
 import { Constructor } from '../utils/interfaces/constructor.interface.ts';
 import { env } from '../utils/functions/env.function.ts';
 import { inject } from '../injector/functions/inject.function.ts';
@@ -12,6 +13,8 @@ import { ServerOptions } from './interfaces/server_options.interface.ts';
 import { WebClientAlias } from './enums/web_client_alias.enum.ts';
 
 export class Server {
+  private readonly config: AppConfig = {};
+
   private readonly defaultHttpPort = 5050;
 
   private readonly listenSignals: Deno.Signal[] = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
@@ -21,6 +24,7 @@ export class Server {
   private readonly router = inject(Router);
 
   constructor(options: ServerOptions) {
+    this.config = options.config ?? {};
     this.modules = options.modules;
   }
 
@@ -47,9 +51,7 @@ export class Server {
   private async serveHttp(connection: Deno.Conn): Promise<void> {
     const httpConnection = Deno.serveHttp(connection);
 
-    for await (const requestEvent of httpConnection) {
-      const { request } = requestEvent;
-
+    for await (const { request, respondWith } of httpConnection) {
       const timerStart = performance.now();
 
       const response = await this.router.respond(request);
@@ -95,7 +97,7 @@ export class Server {
         'color: gray',
       );
 
-      requestEvent.respondWith(response);
+      respondWith(response);
     }
   }
 
@@ -172,7 +174,7 @@ export class Server {
 
     await loadEnv({
       allowEmptyValues: true,
-      envPath: `${Deno.cwd()}/.env`,
+      envPath: `${Deno.cwd()}/${this.config.envFile ?? '.env'}`,
       export: true,
     });
 
