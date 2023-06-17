c269440388198910f1adcf54b3251f7980d5c1b9
dominiq007 <dom.rajkowski@gmail.com>
2023-06-17 21:33:13 +0200

    feat: add more encrypter functionality

--- src/encrypter/encrypter.service.ts
+++ src/encrypter/encrypter.service.ts
@@ -1,13 +1,33 @@
+import { env } from '../utils/functions/env.function.ts';
+
 export class Encrypter {
+  private readonly secretKey = env<string>('CRYPTO_KEY');
+
+  public async compareHash(plainText: string, hash: string): Promise<boolean> {
+    return await this.hash(plainText) === hash;
+  }
+
   public async hash(plainText: string): Promise<string> {
-    const data = new TextEncoder().encode(plainText);
+    const encoder = new TextEncoder();
+    const keyBuffer = encoder.encode(this.secretKey);
+    const dataBuffer = encoder.encode(plainText);
 
-    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    const hmacKey = await crypto.subtle.importKey(
+      'raw',
+      keyBuffer,
+      { name: 'HMAC', hash: 'SHA-256' },
+      false,
+      ['sign'],
+    );
 
-    const hash = [...new Uint8Array(hashBuffer)].map((b) =>
-      b.toString(16).padStart(2, '0')
+    const signature = await crypto.subtle.sign('HMAC', hmacKey, dataBuffer);
+
+    return [...new Uint8Array(signature)].map((byte) =>
+      byte.toString(16).padStart(2, '0')
     ).join('');
+  }
 
-    return hash;
+  public uuid(): string {
+    return crypto.randomUUID();
   }
 }
