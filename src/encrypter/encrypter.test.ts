ca2a9be4543309ae906fda05799958827a668591
dominiq007 <dom.rajkowski@gmail.com>
2023-06-18 20:50:07 +0200

    test: update encrypter test

--- src/encrypter/encrypter.test.ts
+++ src/encrypter/encrypter.test.ts
@@ -5,9 +5,18 @@ import { inject } from '../injector/functions/inject.function.ts';
 Deno.test('encrypter module', async (test) => {
   Deno.env.set('CRYPTO_KEY', 'test');
 
+  const encrypter = inject(Encrypter);
+
   await test.step('encrypter properly hashes a string', async () => {
-    const hash = await inject(Encrypter).hash('test hash');
+    const hash = await encrypter.hash('test');
 
     assertEquals(typeof hash, 'string');
+  });
+
+  await test.step('encrypter properly compares hash with plain text', async () => {
+    const hash = await encrypter.hash('test');
+    const equal = await encrypter.compareHash('test', hash);
+
+    assertEquals(equal, true);
   });
 });
