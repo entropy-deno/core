542319fa6232d2207ff05c4db18365ebaf074a97
dominiq007 <dom.rajkowski@gmail.com>
2023-06-18 19:26:43 +0200

    feat: set default status to http error

--- src/http/http_error.class.ts
+++ src/http/http_error.class.ts
@@ -4,7 +4,7 @@ import { StatusCode } from './enums/status_code.enum.ts';
 export class HttpError extends Error {
   public override readonly name = 'HttpError';
 
-  constructor(public readonly statusCode: StatusCode, customMessage?: string) {
+  constructor(public readonly statusCode = StatusCode.NotFound, customMessage?: string) {
     const message = customMessage ??
       enumKey(statusCode, StatusCode).replace(/([a-z])([A-Z])/g, '$1 $2') ?? 'Error';
 
