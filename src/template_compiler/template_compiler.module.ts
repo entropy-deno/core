8c53103b026bf39e22715e90bddd681d1d4b4c9b
dominiq007 <dom.rajkowski@gmail.com>
2023-07-24 21:27:01 +0200

    feat: don't expose template compiler service

--- src/template_compiler/template_compiler.module.ts
+++ src/template_compiler/template_compiler.module.ts
@@ -1,4 +1,3 @@
 export * from './functions/compile_template.function.ts';
 export * from './functions/render.function.ts';
 export * from './interfaces/template_directive_definition.interface.ts';
-export * from './template_compiler.service.ts';
