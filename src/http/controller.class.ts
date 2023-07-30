0a130fafceff88d6f53b13ac830b2f479f74b601
dominiq007 <dom.rajkowski@gmail.com>
2023-07-30 15:20:58 +0200

    feat: add base controller redirect feature

--- src/http/controller.class.ts
+++ src/http/controller.class.ts
@@ -1,9 +1,18 @@
 import { callerFile } from '../utils/functions/caller_file.function.ts';
+import { HttpStatus } from './enums/http_status.enum.ts';
 import { TemplateCompilerOptions } from '../template_compiler/interfaces/template_compiler_options.interface.ts';
 import { resolveViewFile } from '../template_compiler/functions/resolve_view_file.function.ts';
+import { RoutePath } from '../router/types/route_path.type.ts';
 import { ViewResponse } from './view_response.class.ts';
 
 export class Controller {
+  protected redirectResponse(
+    url: string | RoutePath,
+    statusCode: HttpStatus,
+  ): Response {
+    return Response.redirect(url, statusCode);
+  }
+
   protected renderView(
     file: string,
     variables: Record<string, unknown> = {},
