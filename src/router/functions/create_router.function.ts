93cbf00c91c0e4f81039fb42b01acc374c2e5c35
dominiq007 <dom.rajkowski@gmail.com>
2023-08-01 19:25:11 +0200

    feat: remove createRouter function

--- src/router/functions/create_router.function.ts
+++
@@ -1,6 +0,0 @@
-import { inject } from '../../injector/functions/inject.function.ts';
-import { Router } from '../router.service.ts';
-
-export function createRouter() {
-  return inject(Router);
-}
