262d79c5dc804c491d0af83d4dad840b70bbbbdf
dominiq007 <dom.rajkowski@gmail.com>
2023-09-13 21:38:11 +0200

    feat: develop session api

--- src/http/session.class.ts
+++ src/http/session.class.ts
@@ -1,20 +1,58 @@
-import { exists } from 'https://deno.land/std@0.201.0/fs/mod.ts';
-
 export class Session {
+  private isLoaded = false;
+
+  private readonly variables = new Map<string, unknown>();
+
+  private readonly storagePath = 'storage/sessions';
+
   constructor(private readonly id: string | null) {}
 
-  public async setup(): Promise<void> {
-    const path = `storage/sessions/${this.id}`;
+  private async readData(): Promise<void> {
+    try {
+      const sessionData = JSON.parse(
+        await Deno.readTextFile(`${this.storagePath}/${this.id}.json`),
+      );
 
-    if (!await exists(path)) {
-      await Deno.mkdir(path, {
+      for (const [key, value] of Object.entries(sessionData)) {
+        this.variables.set(key, value);
+      }
+    } catch (error) {
+      if (!(error instanceof Deno.errors.NotFound)) {
+        throw error;
+      }
+    }
+  }
+
+  private async writeDataToFile(): Promise<void> {
+    try {
+      await Deno.mkdir(this.storagePath, {
         recursive: true,
       });
+    } catch (error) {
+      if (!(error instanceof Deno.errors.AlreadyExists)) {
+        throw error;
+      }
     }
 
     await Deno.writeTextFile(
-      `${path}/${this.id}.json`,
-      JSON.stringify({}),
+      `${this.storagePath}/${this.id}.json`,
+      JSON.stringify({ ...this.variables }),
     );
+  }
+
+  public async get<TValue>(key: string): Promise<TValue | null> {
+    if (!this.isLoaded) {
+      await this.readData();
+
+      this.isLoaded = true;
+    }
+
+    return this.variables.get(key) as TValue ?? null;
+  }
+
+  public async set<TValue>(key: string, value: TValue): Promise<void> {
+    this.variables.set(key, value);
+
+    await this.writeDataToFile();
   }
 }
