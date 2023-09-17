c4ca849a4542acb93d58b1e6392c4d8cf9f17951
dominiq007 <dom.rajkowski@gmail.com>
2023-09-17 20:27:04 +0200

    test: add utils test

---
+++ src/utils/utils.test.ts
@@ -0,0 +1,43 @@
+import {
+  assertEquals,
+  assertStringIncludes,
+} from 'https://deno.land/std@0.201.0/assert/mod.ts';
+import { Utils } from './utils.class.ts';
+
+Deno.test('utils module', async (test) => {
+  await test.step('implements properly the callerFile() function', () => {
+    assertStringIncludes(Utils.callerFile(), 'utils.class.ts');
+  });
+
+  await test.step('implements properly the deepMerge() function', () => {
+    const a = {
+      a: 1,
+      b: {
+        c: 2,
+      },
+    };
+
+    const b = {
+      b: {
+        d: 3,
+      },
+    };
+
+    assertEquals(Utils.deepMerge(a, b), {
+      a: 1,
+      b: {
+        c: 2,
+        d: 3,
+      } as Record<string, unknown>,
+    });
+  });
+
+  await test.step('implements properly the escape() function', () => {
+    assertEquals(Utils.escape(`&<>"'`), '&amp;&lt;&gt;&quot;&#39;');
+  });
+
+  await test.step('implements properly the range() function', () => {
+    assertEquals(Utils.range(4, 10), [4, 5, 6, 7, 8, 9, 10]);
+    assertEquals(Utils.range(6), [0, 1, 2, 3, 4, 5, 6]);
+  });
+});
