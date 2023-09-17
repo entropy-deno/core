55a3d09073d208088a3349d93f929bc2ded9f8ce
dominiq007 <dom.rajkowski@gmail.com>
2023-09-17 19:18:29 +0200

    test: update templates test

--- src/templates/templates.test.ts
+++ src/templates/templates.test.ts
@@ -8,14 +8,20 @@ import { TemplateCompiler } from './template_compiler.service.ts';
 Deno.test('templates module', async (test) => {
   const compiler = inject(TemplateCompiler);
 
+  await test.step('compiler properly renders [csrf] directive', async () => {
+    const rendered = await compiler.render('[csrf]');
+
+    assertStringIncludes(rendered, '<input type="hidden"');
+  });
+
   await test.step('compiler properly renders [dev] directive', async () => {
-    const rendered = await compiler.render(`[dev]entropy[/dev]`);
+    const rendered = await compiler.render('[dev]entropy[/dev]');
 
     assertEquals(rendered, 'entropy');
   });
 
   await test.step('compiler properly renders [hotReload] directive', async () => {
-    const rendered = await compiler.render(`[hotReload]`);
+    const rendered = await compiler.render('[hotReload]');
 
     assertStringIncludes(rendered, '<script');
   });
@@ -33,20 +39,20 @@ Deno.test('templates module', async (test) => {
   });
 
   await test.step('compiler properly renders [nonceProp] directive', async () => {
-    const rendered = await compiler.render(`[nonceProp]`);
+    const rendered = await compiler.render('[nonceProp]');
 
     assertStringIncludes(rendered, 'nonce=');
   });
 
   await test.step('compiler properly renders [prod] directive', async () => {
-    const rendered = await compiler.render(`[prod]entropy[/prod]`);
+    const rendered = await compiler.render('[prod]entropy[/prod]');
 
     assertEquals(rendered, '');
   });
 
   await test.step('compiler properly renders [if] directive', async () => {
-    const renderedTrue = await compiler.render(`[if (true)]entropy[/if]`);
-    const renderedFalse = await compiler.render(`[if (false)]entropy[/if]`);
+    const renderedTrue = await compiler.render('[if (true)]entropy[/if]');
+    const renderedFalse = await compiler.render('[if (false)]entropy[/if]');
 
     assertEquals(renderedTrue, 'entropy');
     assertEquals(renderedFalse, '');
@@ -54,9 +60,17 @@ Deno.test('templates module', async (test) => {
 
   await test.step('compiler properly renders [each] directive', async () => {
     const rendered = await compiler.render(
-      `[each (item in [1, 2, 3])]{{ item }};[/each]`,
+      '[each (item in [1, 2, 3])]{{ item }};[/each]',
     );
 
     assertEquals(rendered, '1;2;3;');
+  });
+
+  await test.step('compiler properly renders [stack] and [push] directive', async () => {
+    const rendered = await compiler.render(
+      `[stack('test')][push('test')]a[/push][push('test')]b[/push]`,
+    );
+
+    assertStringIncludes(rendered, 'ab');
   });
 });
