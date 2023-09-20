86deaad42188dfb63374b3b2cf62eafb9eab407e
dominiq007 <dom.rajkowski@gmail.com>
2023-09-20 20:52:26 +0200

    test: add basic configurator test

---
+++ src/configurator/configurator.test.ts
@@ -0,0 +1,15 @@
+import { assertEquals } from 'https://deno.land/std@0.202.0/assert/mod.ts';
+import { Configurator } from './configurator.service.ts';
+import { inject } from '../injector/functions/inject.function.ts';
+
+const configurator = inject(Configurator);
+
+Deno.test('configurator module', async (test) => {
+  await test.step('implements properly .env parsing', () => {
+    assertEquals(configurator.getEnv('TESTING'), true);
+  });
+
+  await test.step('implements properly configuration parsing', () => {
+    assertEquals(configurator.entries.host, 'localhost');
+  });
+});
