e2b6ebbecc531d4cfa1292c9f471042e7d786653
dominiq007 <dom.rajkowski@gmail.com>
2023-10-04 21:12:37 +0200

    test: check template compiles switch directive rendering

--- src/templates/templates.test.ts
+++ src/templates/templates.test.ts
@@ -20,10 +20,28 @@ Deno.test('templates module', async (test) => {
     assertStringIncludes(rendered, 'entropy');
   });
 
+  await test.step('compiler properly renders @each directive', async () => {
+    const rendered = await compiler.render(
+      '@each (item in [1, 2, 3]) {{ item }} @/each',
+    );
+
+    assertStringIncludes(rendered, '1');
+    assertStringIncludes(rendered, '2');
+    assertStringIncludes(rendered, '3');
+  });
+
   await test.step('compiler properly renders @hotReload directive', async () => {
     const rendered = await compiler.render('@hotReload');
 
     assertStringIncludes(rendered, '<script');
+  });
+
+  await test.step('compiler properly renders @if directive', async () => {
+    const renderedTrue = await compiler.render('@if (true) entropy @/if');
+    const renderedFalse = await compiler.render('@if (false) entropy @/if');
+
+    assertStringIncludes(renderedTrue, 'entropy');
+    assertEquals(renderedFalse, '');
   });
 
   await test.step('compiler properly renders @json directive', async () => {
@@ -50,24 +68,6 @@ Deno.test('templates module', async (test) => {
     assertEquals(rendered, '');
   });
 
-  await test.step('compiler properly renders @if directive', async () => {
-    const renderedTrue = await compiler.render('@if (true) entropy @/if');
-    const renderedFalse = await compiler.render('@if (false) entropy @/if');
-
-    assertStringIncludes(renderedTrue, 'entropy');
-    assertEquals(renderedFalse, '');
-  });
-
-  await test.step('compiler properly renders @each directive', async () => {
-    const rendered = await compiler.render(
-      '@each (item in [1, 2, 3]) {{ item }} @/each',
-    );
-
-    assertStringIncludes(rendered, '1');
-    assertStringIncludes(rendered, '2');
-    assertStringIncludes(rendered, '3');
-  });
-
   await test.step('compiler properly renders @stack and @push directive', async () => {
     const rendered = await compiler.render(
       `
@@ -80,5 +80,27 @@ Deno.test('templates module', async (test) => {
 
     assertStringIncludes(rendered, 'a');
     assertStringIncludes(rendered, 'b');
+  });
+
+  await test.step('compiler properly renders @switch directive', async () => {
+    const rendered = await compiler.render(
+      `
+      @switch (2)
+        @case (1)
+          1
+        @/case
+
+        @case (2)
+          2
+        @/case
+
+        @case (3)
+          3
+        @/case
+      @/switch
+      `,
+    );
+
+    assertStringIncludes(rendered, '2');
   });
 });
