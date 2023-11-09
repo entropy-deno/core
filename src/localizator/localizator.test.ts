566f49a6f87da5da5fc9b2899fe2192d85538eaf
dominiq007 <dom.rajkowski@gmail.com>
2023-11-09 19:43:41 +0100

    test: add localizator test

---
+++ src/localizator/localizator.test.ts
@@ -0,0 +1,19 @@
+import { assertEquals } from 'https://deno.land/std@0.205.0/assert/mod.ts';
+import { Localizator } from './localizator.service.ts';
+import { inject } from '../injector/functions/inject.function.ts';
+
+Deno.test('localizator module', async (test) => {
+  const localizator = inject(Localizator);
+
+  await test.step('localizator properly sets translations', () => {
+    localizator.set('pl', 'hello world', 'witaj świecie');
+
+    assertEquals(localizator.translate('pl', 'hello world'), 'witaj świecie');
+  });
+
+  await test.step('localizator properly returns translation list', () => {
+    assertEquals(localizator.all('pl'), {
+      'hello world': 'witaj świecie',
+    });
+  });
+});
