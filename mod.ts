7a93b0a6476752547cbad335f866ff1b2682eda7
dominiq007 <dom.rajkowski@gmail.com>
2023-11-13 21:08:53 +0100

    feat: move module entry to the parent directory

---
+++ mod.ts
@@ -0,0 +1,17 @@
+export * from './src/constants.ts';
+
+export * from './src/configurator/configurator.module.ts';
+export * from './src/encrypter/encrypter.module.ts';
+export * from './src/http/http.module.ts';
+export * from './src/injector/injector.module.ts';
+export * from './src/localizator/localizator.module.ts';
+export * from './src/logger/logger.module.ts';
+export * from './src/pipes/pipes.module.ts';
+export * from './src/router/router.module.ts';
+export * from './src/scheduler/scheduler.module.ts';
+export * from './src/server/server.module.ts';
+export * from './src/templates/templates.module.ts';
+export * from './src/testing/testing.module.ts';
+export * from './src/utils/utils.module.ts';
+export * from './src/validator/validator.module.ts';
+export * from './src/web_socket/web_socket.module.ts';
