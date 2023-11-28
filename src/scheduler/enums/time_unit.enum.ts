8ced7852c87b4f44efb215cee9825d13cdf9856d
dominiq007 <dom.rajkowski@gmail.com>
2023-11-28 20:24:45 +0100

    feat: add time unit enum for scheduler

---
+++ src/scheduler/enums/time_unit.enum.ts
@@ -0,0 +1,9 @@
+export enum TimeUnit {
+  Second = 1000,
+  Minute = 60 * Second,
+  Hour = 60 * Minute,
+  Day = 24 * Hour,
+  Week = 7 * Day,
+  Month = 30 * Day,
+  Year = 365 * Day,
+}
--- src/scheduler/scheduler.module.ts
+++ src/scheduler/scheduler.module.ts
@@ -1,4 +1,5 @@
 export * from './decorators/interval.decorator.ts';
 export * from './decorators/schedule.decorator.ts';
 export * from './decorators/timeout.decorator.ts';
+export * from './enums/time_unit.enum.ts';
 export * from './scheduler.service.ts';
