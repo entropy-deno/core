752d5bd921a1c9682b8b458ea49fc8e68d3378d0
dominiq007 <dom.rajkowski@gmail.com>
2024-03-28 15:41:38 +0100

    chore: bump required deno version

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const DENO_VERSION = Deno.version.deno;
-export const MIN_DENO_VERSION = '1.40.0';
+export const MIN_DENO_VERSION = '1.42.0';
 export const OS = Deno.build.os;
 export const TYPESCRIPT_VERSION = Deno.version.typescript;
 export const V8_VERSION = Deno.version.v8;
